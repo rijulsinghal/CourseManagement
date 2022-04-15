4e841ce00e7dfd398a682764fa2ba507b521e8ae
rijulsinghal <rijulsinghal63@gmail.com>
2022-04-16 00:35:40 +0530

    Added Login/SignUp Templates (Working Fine)

---
+++ coursemanagement/src/main/java/com/coursemanagement/coursemanagement/controller/SignUp.java
@@ -0,0 +1,14 @@
+package com.coursemanagement.coursemanagement.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SignUp {
+
+    @GetMapping({"/signup"})
+    public void signup(){
+
+    }
+    
+}

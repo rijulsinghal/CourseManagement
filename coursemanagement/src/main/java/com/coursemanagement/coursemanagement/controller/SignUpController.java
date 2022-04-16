adc286879cc57df599ef4f093e306d6ff7d5c9e6
rijulsinghal <rijulsinghal63@gmail.com>
2022-04-16 19:01:11 +0530

    Model and Controller Package Added

--- coursemanagement/src/main/java/com/coursemanagement/coursemanagement/controller/SignUpController.java
+++ coursemanagement/src/main/java/com/coursemanagement/coursemanagement/controller/SignUpController.java
@@ -4,10 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class SignUp {
+public class SignUpController {
 
     @GetMapping({"/signup"})
     public void signup(){
+
+        
 
     }
     
